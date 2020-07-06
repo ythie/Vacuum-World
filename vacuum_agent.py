d9902dba3234ac455eca8652ba272c1ea72d0fdf
SamDaQueen <samreenreyaz@outlook.com>
2020-07-07 03:10:39 +0530

    Rearrangement changes

--- vacuum_agent.py
+++ vacuum_agent.py
@@ -1,160 +1,166 @@
-#This file contains the program for a vacuum agent in a continuum vacuum world.
+# This file contains the program for a vacuum agent in a continuum vacuum world
 
 import random
 
-'''
-This class represents the observable world at any given instance.
-It contains functions for performing an action and printing the world grid.
-'''
+
 class ContinuumWorld:
-
     '''
-    This function will initialize an object for the ContinuumWorld class.
-
-    Arguments:
-        grid_size - a list [x,y] with size of the world
-        dirt - a list of lists representing the entire world with the dirt value
-            of each tile
+    This class represents the observable world at any given instance.
+    It contains functions for performing an action and printing the world grid.
     '''
-    def __init__(self, grid_size=[2,2], dirt=[[1,1],[1,1]]):
+
+    def __init__(self, grid_size=[2, 2], dirt=[[1, 1], [1, 1]]):
+        '''
+        This function will initialize an object for the ContinuumWorld class.
+
+        Arguments:
+            grid_size - a list [x,y] with size of the world
+            dirt - a list of lists representing the entire world with the dirt
+            value
+                of each tile
+        '''
         self.grid_size = grid_size
         self.dirt = dirt
 
-    '''
-    This function will change the position of the agent when movement actions
-    are performed, and will change the tile to clean on suck action
-
-    Arguments:
-        agent - the vacuum agent object
-        action - the action performed
-    '''
-    def perform_action(self, agent, action):
-        if action=='R':
-            agent.position[1] += 1
-        if action=='L':
-            agent.position[1] -= 1
-        if action=='U':
-            agent.position[0] -= 1
-        if action=='D':
-            agent.position[0] += 1
-        if action=='S':
-            agent.score += self.dirt[agent.position[0]][agent.position[1]]
-            self.dirt[agent.position[0]][agent.position[1]] = 0
-
-    '''
-    This function prints the current world representation with dirt in each tile
-
-    Arguments:
-        agent - the vacuum agent object
-        position - the current position of agent
-    '''
     def print_dirt(self, agent, position):
-        #print(agent.position)
+        '''
+        This function prints the current world representation with dirt in each
+        tile
+
+        Arguments:
+            agent - the vacuum agent object
+            position - the current position of agent
+        '''
+        # print(agent.position)
         part = self.dirt[:agent.position[0]]
         print()
         for row in part:
-            print(*row, sep = ", ")
+            print(*row, sep=", ")
 
         current = self.dirt[agent.position[0]]
-        print(*current[:agent.position[1]], sep=", ",end=" ")
-        print("["+str(self.dirt[agent.position[0]][agent.position[1]])+"]",end=" ")
+        print(*current[:agent.position[1]], sep=", ", end=" ")
+        print("["+str(self.dirt[agent.position[0]][agent.position[1]])
+              + "]", end=" ")
         print(*current[agent.position[1]+1:], sep=", ")
 
         part = self.dirt[agent.position[0]+1:]
         for row in part:
-            print(*row, sep = ", ")
+            print(*row, sep=", ")
         print()
 
-'''
-This class is for the functions of the vacuum agent.
-It contains functions for verifying that it does not cross boundary and for
-cleaning the world.
-'''
+
 class SimpleReflexAgent:
-
     '''
-    This function will initialize an object for the SimpleReflexAgent class.
-
-    Arguments:
-        position - the initial position of agent
-        max_moves - the maximum number of moves agent is allowed to take
-        grid_size - a list [x,y] with size of the world
-        dirt - a list of lists representing the entire world with the dirt value
-            of each tile
+    This class is for the functions of the vacuum agent.
+    It contains functions for verifying that it does not cross boundary and for
+    cleaning the world.
     '''
-    def __init__(self, position=[0,0], max_moves=30, grid_size=0, dirt=0):
+
+    def __init__(self, position=[0, 0], max_moves=30, grid_size=0, dirt=0):
+        '''
+        This function will initialize an object for the SimpleReflexAgent class
+
+        Arguments:
+            position - the initial position of agent
+            max_moves - the maximum number of moves agent is allowed to take
+            grid_size - a list [x,y] with size of the world
+            dirt - a list of lists representing the entire world with the dirt
+            value of each tile
+        '''
         self.score = 0
         self.position = position
         self.max_moves = 30
         self.world = ContinuumWorld(grid_size, dirt)
 
-    '''
-    This function checks if step taken by the agent will cross boundary.
-
-    Arguments:
-        step - any one from 'R', 'L', 'U' and 'D'
-
-    Returns: True if boundary will be crossed
-    '''
     def crosses_boundary(self, step):
-        error = "Cannot go beyond! Change direction!\n"
-        if step=='R':
-            if self.position[1]+1>self.world.grid_size[1]-1:
-                #print(error)
+        '''
+        This function checks if step taken by the agent will cross boundary.
+
+        Arguments:
+            step - any one from 'R', 'L', 'U' and 'D'
+
+        Returns: True if boundary will be crossed
+        '''
+        # error = "Cannot go beyond! Change direction!\n"
+        if step == 'R':
+            if self.position[1]+1 > self.world.grid_size[1]-1:
+                # print(error)
                 return True
-        if step=='L':
-            if self.position[1]-1<0:
-                #print(error)
+        if step == 'L':
+            if self.position[1]-1 < 0:
+                # print(error)
                 return True
-        if step=='U':
-            if self.position[0]-1<0:
-                #print(error)
+        if step == 'U':
+            if self.position[0]-1 < 0:
+                # print(error)
                 return True
-        if step=='D':
-            if self.position[0]+1>self.world.grid_size[0]-1:
-                #print(error)
+        if step == 'D':
+            if self.position[0]+1 > self.world.grid_size[0]-1:
+                # print(error)
                 return True
 
-    '''
-    This function is to tell the agent to move around and clean the tiles.
-    '''
+    def perform_action(self, action):
+        '''
+        This function will change the position of the agent when movement
+        actions are performed, and will change the tile to clean on suck action
+
+        Arguments:
+            agent - the vacuum agent object
+            action - the action performed
+        '''
+        if action == 'R':
+            self.position[1] += 1
+        if action == 'L':
+            self.position[1] -= 1
+        if action == 'U':
+            self.position[0] -= 1
+        if action == 'D':
+            self.position[0] += 1
+        if action == 'S':
+            self.score += self.world.dirt[self.position[0]][self.position[1]]
+            self.world.dirt[self.position[0]][self.position[1]] = 0
+
     def clean_grid(self):
+        '''
+        This function is to tell the agent to move around and clean the tiles.
+        '''
         counter = 0
         for i in range(self.max_moves):
             counter += 1
-            if self.world.dirt[self.position[0]][self.position[1]]>0:
+            if self.world.dirt[self.position[0]][self.position[1]] > 0:
                 step = 'S'
             else:
                 while(True):
-                    step = random.choice(['R','L','U','D'])
+                    step = random.choice(['R', 'L', 'U', 'D'])
                     if self.crosses_boundary(step):
                         continue
                     else:
                         break
 
-            self.world.perform_action(self, step)
-            #print("Step number:", counter)
-            print(step, round(self.score,5))
+            self.perform_action(step)
+            # print("Step number:", counter)
+            print(step, round(self.score, 5))
 
-            if counter%5 == 0:
+            if counter % 5 == 0:
                 self.world.print_dirt(self, self.position)
 
-'''
-This function reads the 'environ.txt' file to retrieve initital state of world
-and agent.
 
-Arguments:
-    text_file - the text file to fetch the information from
-
-Returns:
-    pos - the initial position of agent
-    moves - the maximum number of moves agent is allowed to take
-    grid - a list [x,y] with size of the world
-    dirt - a list of lists representing the entire world with the dirt value
-        of each tile
-'''
 def read_file(text_file):
-    with open(text_file,'r') as ev:
+    '''
+    This function reads the 'environ.txt' file to retrieve initital state of
+    world and agent.
+
+    Arguments:
+        text_file - the text file to fetch the information from
+
+    Returns:
+        pos - the initial position of agent
+        moves - the maximum number of moves agent is allowed to take
+        grid - a list [x,y] with size of the world
+        dirt - a list representing the entire world with the dirt value of each
+            tile
+    '''
+    with open(text_file, 'r') as ev:
         e = ev.readlines()
 
     grid = []
@@ -164,7 +170,7 @@ def read_file(text_file):
     dirt = e[2:2+grid[0]]
     dirt = [[float(num) for num in row.split(' ')] for row in dirt]
 
-    moves = int(e[10].replace('MOVES: ',''))
+    moves = int(e[10].replace('MOVES: ', ''))
 
     pos = []
     for i in e[11][e[11].find('INITIAL')+8:e[0].find('GRID')+15].split():
@@ -178,6 +184,7 @@ def read_file(text_file):
 
     return pos, moves, grid, dirt
 
+
 def main():
 
     pos, moves, grid, dirt = read_file('environ.txt')
@@ -185,5 +192,6 @@ def main():
     agent = SimpleReflexAgent(pos, moves, grid, dirt)
     agent.clean_grid()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
