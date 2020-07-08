4aa846d937f986ab07fc2520c02df99faa9d5775
SamDaQueen <samreenreyaz@outlook.com>
2020-07-08 06:58:44 +0530

    Docstrings improved

--- vacuum_agent.py
+++ vacuum_agent.py
@@ -4,30 +4,31 @@ import random
 
 
 class ContinuumWorld:
-    '''
+    """
     This class represents the observable world at any given instance.
     It contains functions for performing an action and printing the world grid.
-    '''
+    """
 
     def __init__(self, grid_size=[2, 2], dirt=[[1, 1], [1, 1]]):
-        '''
-        This function will initialize an object for the ContinuumWorld class.
-        Arguments:
-            grid_size - a list [x,y] with size of the world
-            dirt - a list of lists representing the entire world with the dirt
-            value of each tile
-        '''
+        """This function will initialize an object for the ContinuumWorld class.
+
+        Args:
+            grid_size (list, optional): a list [x,y] with size of the world.
+             Defaults to [2, 2].
+            dirt (list, optional): a list of lists representing the entire
+            world with the dirt value of each tile. Defaults to [[1, 1],[1, 1]].
+        """
         self.grid_size = grid_size
         self.dirt = dirt
 
     def print_dirt(self, agent, position):
-        '''
-        This function prints the current world representation with dirt in each
-        tile
-        Arguments:
-            agent - the vacuum agent object
-            position - the current position of agent
-        '''
+        """This function prints the current world representation with dirt in
+        each tile
+
+        Args:
+            agent (Agent): the vacuum agent object,
+            position (list): the current position of agent
+        """
         # print(agent.position)
         part = self.dirt[:agent.position[0]]
         print()
@@ -47,23 +48,26 @@ class ContinuumWorld:
 
 
 class Agent:
-    '''
+    """
     This class is for the basic description of the vacuum agent.
     It contains functions for verifying that it does not cross boundary,
     performing a moving or sucking action, and getting neighbor with maximum
     dirt.
-    '''
+    """
 
     def __init__(self, position=[0, 0], max_moves=30, grid_size=0, dirt=0):
-        '''
-        This function will initialize an object for the SimpleReflexAgent class
-        Arguments:
-            position - the initial position of agent
-            max_moves - the maximum number of moves agent is allowed to take
-            grid_size - a list [x,y] with size of the world
-            dirt - a list of lists representing the entire world with the dirt
-            value of each tile
-        '''
+        """This function will initialize an object for the Agent class
+
+        Args:
+            position (list, optional): the initial position of agent.
+            Defaults to [0, 0].
+            max_moves (int, optional): the maximum number of moves agent is
+            allowed to take. Defaults to 30.
+            grid_size (int, optional): a list [x,y] with size of the world.
+            Defaults to 0.
+            dirt (int, optional): a list of lists representing the entire
+            world with the dirt value of each tile. Defaults to 0.
+        """
         self.score = 0
         self.position = position
         self.max_moves = max_moves
@@ -72,12 +76,14 @@ class Agent:
         self.visited.add((self.position[0], self.position[1]))
 
     def crosses_boundary(self, step):
-        '''
-        This function checks if step taken by the agent will cross boundary.
-        Arguments:
-            step - any one from 'R', 'L', 'U' and 'D'
-        Returns: True if boundary will be crossed
-        '''
+        """This function checks if step taken by the agent will cross boundary.
+
+        Args:
+            step (String): any one from 'R', 'L', 'U' and 'D'
+
+        Returns:
+            boolean: True if boundary will be crossed
+        """
         # error = "Cannot go beyond! Change direction!\n"
         if step == 'R':
             if self.position[1]+1 > self.world.grid_size[1]-1:
@@ -99,13 +105,12 @@ class Agent:
         return False
 
     def perform_action(self, action):
-        '''
-        This function will change the position of the agent when movement
+        """This function will change the position of the agent when movement
         actions are performed, and will change the tile to clean on suck action
-        Arguments:
-            agent - the vacuum agent object
-            action - the action performed
-        '''
+
+        Args:
+            action (String): the action to be performed
+        """
         if action == 'R':
             self.position[1] += 1
         if action == 'L':
@@ -120,7 +125,15 @@ class Agent:
 
         self.visited.add((self.position[0], self.position[1]))
 
-    def print_status(self, step, counter):
+    def print_status(self, step, counter=0):
+        """This function prints the step and performance measure of agent at
+        every step, and prints the world grid with current position of agent
+        at every 5 steps.
+
+        Args:
+            step (String): direction or suck action taken by agent
+            counter (int): counts the number of steps completed by agent
+        """
         print(step, round(self.score, 5))
         # print(self.visited)
         if counter % 5 == 0:
@@ -128,20 +141,20 @@ class Agent:
 
 
 class SimpleReflexAgent(Agent):
-    '''
-    This reflex agent has no memory, no state and can only sense the present
+    """This reflex agent has no memory, no state and can only sense the present
     tile. It can sense the value of dirt present in and also if any of the
     boundary edges are walls.
-    It contains functions for moving aroud cleaning the world.
-    '''
+    It has functions for moving aroud cleaning the world.
+
+    Args:
+        Agent (Agent): The parent class
+    """
 
     def clean_grid(self):
-        '''
-        This function is to tell the agent to move around and clean the tiles.
-        '''
-        counter = 0
+        """This function is to tell the agent to move around and clean the
+        tiles.
+        """
         for i in range(self.max_moves):
-            counter += 1
             if self.world.dirt[self.position[0]][self.position[1]] > 0:
                 step = 'S'
             else:
@@ -153,40 +166,41 @@ class SimpleReflexAgent(Agent):
                         break
 
             self.perform_action(step)
-            # print("Step number:", counter)
-            self.print_status(step, counter)
+            # print("Step number:", i)
+            self.print_status(step, i)
 
 
 class GreedyAgent(Agent):
-    '''
-    This reflex agent has no memory, no state and can sense its
+    """This reflex agent has no memory, no state and can sense its
     neighboring tiles. It can sense the value of dirt present in and also if
     any of the boundary edges are walls.
     It contains functions for moving aroud cleaning the world.
-    '''
+
+    Args:
+        Agent (Agent): The parent class
+    """
 
     def clean_grid(self):
-        '''
-        This function is to tell the agent to move around and clean the tiles.
-        '''
-        counter = 0
+        """This function is to tell the agent to move around and clean the
+        tiles.
+        """
         for i in range(self.max_moves):
-            counter += 1
             if self.world.dirt[self.position[0]][self.position[1]] > 0:
                 step = 'S'
             else:
                 step = self.get_max_neighbor()
 
             self.perform_action(step)
-            # print("Step number:", counter)
-            self.print_status(step, counter)
+            # print("Step number:", i)
+            self.print_status(step, i)
 
     def get_max_neighbor(self):
-        '''
-        This function checks which neighboring tile of agent has maximum dirt.
-        Return:
-            direction in which maximum dirt is present, 'R', 'L', 'U' or 'D'
-        '''
+        """This function checks which neighboring tile of agent has maximum
+        dirt.
+
+        Returns:
+            String: the neighbor with maximum dirt value
+        """
         neighbor = {}
         y = self.position[0]
         x = self.position[1]
@@ -206,36 +220,36 @@ class GreedyAgent(Agent):
 
 
 class StateAgent(Agent):
-    '''
-    This reflex agent has memory, state and can sense its neighboring tiles.
+    """This reflex agent has memory, state and can sense its neighboring tiles.
     It can sense the value of dirt present in and also if any of the boundary
     edges are walls.
     It contains functions for moving aroud cleaning the world.
-    '''
+
+    Args:
+        Agent (Agent): The parent class
+    """
 
     def clean_grid(self):
-        '''
-        This function is to tell the agent to move around and clean the tiles.
-        '''
-        counter = 0
+        """This function is to tell the agent to move around and clean the
+        tiles.
+        """
         for i in range(self.max_moves):
-            counter += 1
             if self.world.dirt[self.position[0]][self.position[1]] > 0:
                 step = 'S'
             else:
                 step = self.get_best_neighbor()
 
             self.perform_action(step)
-            # print("Step number:", counter)
-            self.print_status(step, counter)
+            # print("Step number:", i)
+            self.print_status(step, i)
 
     def get_best_neighbor(self):
-        '''
-        This function checks which neighboring tile of agent has maximum dirt
+        """This function checks which neighboring tile of agent has maximum dirt
         and if that tile has been visited before.
-        Return:
-            direction in which maximum dirt is present, 'R', 'L', 'U' or 'D'
-        '''
+
+        Returns:
+            String: the neighbor with maximum dirt value
+        """
         neighbor = {}
         y = self.position[0]
         x = self.position[1]
@@ -269,18 +283,19 @@ class StateAgent(Agent):
 
 
 def read_file(text_file):
-    '''
-    This function reads the 'environ.txt' file to retrieve initital state of
+    """This function reads the 'environ.txt' file to retrieve initital state of
     world and agent.
-    Arguments:
-        text_file - the text file to fetch the information from
+
+    Args:
+        text_file (file): the text file to fetch the information from
+
     Returns:
-        pos - the initial position of agent
-        moves - the maximum number of moves agent is allowed to take
-        grid - a list [x,y] with size of the world
-        dirt - a list representing the entire world with the dirt value of each
-            tile
-    '''
+        pos [list]: the initial position of agent
+        moves [int]: the maximum number of moves agent is allowed to take
+        grid [list]: a list [x,y] with size of the world
+        dirt [list[list[]]]: a list representing the entire world with the dirt
+        value of eachtile
+    """
     with open(text_file, 'r') as ev:
         e = ev.readlines()
 
@@ -308,7 +323,7 @@ def read_file(text_file):
 
 def main():
 
-    seed = 1
+    seed = 1  # for passing same random parameters to all three agents
 
     pos, moves, grid, dirt = read_file('environ.txt')
     # pos = [6, 0]
